babe14f9937c98a074880a3cd1a8975e07e57a2c
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2022-04-16 02:19:58 +0900

    [AC] SubmissionID: 30989243 at 2022-04-16 02:19:58 (1650043198)

---
+++ abs/practice_1_30989243_AC.cpp
@@ -0,0 +1,21 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/abs/submissions/30989243
+ * Submitted at: 2022-04-16 02:19:58
+ * Problem URL: https://atcoder.jp/contests/abs/tasks/practice_1
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <iostream>
+int main()
+{
+	int a;
+  	std::cin >> a;
+	int b, c;
+  	std::cin >> b >> c;
+  	std::string s;
+  	std::cin >> s;
+  	std::cout << a + b + c << " " << s << std::endl;
+	return 0;
+}
