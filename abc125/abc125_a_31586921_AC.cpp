f8e719be4749bdf31a440ccd7a688b317f428951
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2022-05-10 19:22:36 +0900

    [AC] SubmissionID: 31586921 at 2022-05-10 19:22:36 (1652178156)

---
+++ abc125/abc125_a_31586921_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/abc125/submissions/31586921
+ * Submitted at: 2022-05-10 19:22:36
+ * Problem URL: https://atcoder.jp/contests/abc125/tasks/abc125_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int A, B, T;
+    cin >> A >> B >> T;
+    cout << (int)((T + 0.5) / A) * B << endl;
+}
