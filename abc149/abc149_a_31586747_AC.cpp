8faf3b0c6bf9f02d4b3e7e4da5fca6698f480dc0
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2022-05-10 19:10:47 +0900

    [AC] SubmissionID: 31586747 at 2022-05-10 19:10:47 (1652177447)

---
+++ abc149/abc149_a_31586747_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/abc149/submissions/31586747
+ * Submitted at: 2022-05-10 19:10:47
+ * Problem URL: https://atcoder.jp/contests/abc149/tasks/abc149_a
+ * Result: AC
+ * Execution Time: 9 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string S, T;
+    cin >> S >> T;
+    cout << T + S << endl;
+}
