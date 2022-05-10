9f0b2c8fabb2a55036aca5da9242d9e54da1f19c
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2022-05-10 19:01:15 +0900

    [WA] SubmissionID: 31586627 at 2022-05-10 19:01:15 (1652176875)

---
+++ APG4b/APG4b_cr_31586627_WA.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/APG4b/submissions/31586627
+ * Submitted at: 2022-05-10 19:01:15
+ * Problem URL: https://atcoder.jp/contests/APG4b/tasks/APG4b_cr
+ * Result: WA
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a, b;
+  cin >> a, b;
+  cout << a + b << endl;
+}
