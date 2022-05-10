6a3562769636ffb76612ee3e316e3afc0667dc53
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2022-05-10 19:00:13 +0900

    [AC] SubmissionID: 31586612 at 2022-05-10 19:00:13 (1652176813)

---
+++ APG4b/APG4b_cs_31586612_AC.cpp
@@ -0,0 +1,22 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/APG4b/submissions/31586612
+ * Submitted at: 2022-05-10 19:00:13
+ * Problem URL: https://atcoder.jp/contests/APG4b/tasks/APG4b_cs
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  // 一年の秒数
+  int seconds = 365 * 24 * 60 * 60;
+ 
+  // 以下のコメント/* */を消して追記する
+  cout << seconds << endl;
+  cout << seconds * 2 << endl;
+  cout << seconds * 5 << endl;
+  cout << seconds * 10 << endl;
+}
