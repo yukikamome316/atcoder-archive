13c09b6a19d25a91c97b32a35cef57b23ddd0090
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2022-05-10 18:56:36 +0900

    [AC] SubmissionID: 31586571 at 2022-05-10 18:56:36 (1652176596)

---
+++ APG4b/APG4b_ct_31586571_AC.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/APG4b/submissions/31586571
+ * Submitted at: 2022-05-10 18:56:36
+ * Problem URL: https://atcoder.jp/contests/APG4b/tasks/APG4b_ct
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << (100 * (100 + 1)) / 2 << endl;
+    return 0;
+}
