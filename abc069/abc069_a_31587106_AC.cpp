7e6fba712342e58a3cd97053083f177c3106f5ad
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2022-05-10 19:35:22 +0900

    [AC] SubmissionID: 31587106 at 2022-05-10 19:35:22 (1652178922)

---
+++ abc069/abc069_a_31587106_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/abc069/submissions/31587106
+ * Submitted at: 2022-05-10 19:35:22
+ * Problem URL: https://atcoder.jp/contests/abc069/tasks/abc069_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    cout << (n - 1) * (m - 1) << endl;
+}
