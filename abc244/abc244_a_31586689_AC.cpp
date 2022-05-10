3085c7d8626b0ade974548e31a62ffbdee7077c3
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2022-05-10 19:06:50 +0900

    [AC] SubmissionID: 31586689 at 2022-05-10 19:06:50 (1652177210)

---
+++ abc244/abc244_a_31586689_AC.cpp
@@ -0,0 +1,18 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/abc244/submissions/31586689
+ * Submitted at: 2022-05-10 19:06:50
+ * Problem URL: https://atcoder.jp/contests/abc244/tasks/abc244_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    string S;
+    cin >> N >> S;
+    cout << S[N - 1] << endl;
+}
