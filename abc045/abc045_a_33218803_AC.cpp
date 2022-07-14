ccf2b221160be7931e85dff2f37959fdedc6b806
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2022-07-14 18:16:27 +0900

    [AC] SubmissionID: 33218803 at 2022-07-14 18:16:27 (1657790187)

---
+++ abc045/abc045_a_33218803_AC.cpp
@@ -0,0 +1,19 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/abc045/submissions/33218803
+ * Submitted at: 2022-07-14 18:16:27
+ * Problem URL: https://atcoder.jp/contests/abc045/tasks/abc045_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+
+using namespace std;
+
+int main() {
+    int a, b, h;
+    cin >> a >> b >> h;
+    cout << (a + b) * h / 2 << endl;
+}
