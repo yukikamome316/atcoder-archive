45dcc69c0fc2a84ac9a23e08e5c06e6c3c0c46f4
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2022-07-21 18:20:46 +0900

    [AC] SubmissionID: 33392768 at 2022-07-21 18:20:46 (1658395246)

---
+++ abc151/abc151_a_33392768_AC.cpp
@@ -0,0 +1,21 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/abc151/submissions/33392768
+ * Submitted at: 2022-07-21 18:20:46
+ * Problem URL: https://atcoder.jp/contests/abc151/tasks/abc151_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (int)n; i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+    char c; cin >> c;
+    cout << (char)((int)c + 1) << endl;
+}
