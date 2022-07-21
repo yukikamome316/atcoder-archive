b41365c7ce2c8c2f03bd439f0134c391aaddbaaa
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2022-07-22 02:18:52 +0900

    [AC] SubmissionID: 33399562 at 2022-07-22 02:18:52 (1658423932)

---
+++ typical90/typical90_b_33399562_AC.cpp
@@ -0,0 +1,46 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/typical90/submissions/33399562
+ * Submitted at: 2022-07-22 02:18:52
+ * Problem URL: https://atcoder.jp/contests/typical90/tasks/typical90_b
+ * Result: AC
+ * Execution Time: 90 ms
+ */
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (int)n; i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+    int N; cin >> N;
+    vector<string> result;
+    rep(i, 1 << N) {
+        int depth = 0;
+        string S;
+        bool flag = false;
+        rep(j, N) {
+            if (i & (1 << j)) {
+                depth++;
+                S += "(";
+            } else {
+                depth--;
+                S += ")";
+            }
+            if (depth < 0) {
+                flag = true;
+                break;
+            }
+        }
+        if (flag) continue;
+        if (!depth) 
+            result.emplace_back(S);
+    }
+
+    sort(result.begin(), result.end());
+    for (string &str : result)
+        cout << str << endl;
+}
