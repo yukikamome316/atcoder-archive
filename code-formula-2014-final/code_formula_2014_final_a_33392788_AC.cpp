07e2de10dc6609a2a03ed97d14e28c89bb3faf73
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2022-07-21 18:21:59 +0900

    [AC] SubmissionID: 33392788 at 2022-07-21 18:21:59 (1658395319)

---
+++ code-formula-2014-final/code_formula_2014_final_a_33392788_AC.cpp
@@ -0,0 +1,21 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/code-formula-2014-final/submissions/33392788
+ * Submitted at: 2022-07-21 18:21:59
+ * Problem URL: https://atcoder.jp/contests/code-formula-2014-final/tasks/code_formula_2014_final_a
+ * Result: AC
+ * Execution Time: 9 ms
+ */
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (int)n; i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+    int A, B; cin >> A >> B;
+    cout << A * B << endl;
+}
