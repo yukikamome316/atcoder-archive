df32c4190606d02aa686f1a8a946b4802c5b5f80
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2022-07-24 13:32:31 +0900

    [AC] SubmissionID: 33489140 at 2022-07-24 13:32:31 (1658637151)

---
+++ abc261/abc261_d_33489140_AC.cpp
@@ -0,0 +1,43 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/abc261/submissions/33489140
+ * Submitted at: 2022-07-24 13:32:31
+ * Problem URL: https://atcoder.jp/contests/abc261/tasks/abc261_d
+ * Result: AC
+ * Execution Time: 181 ms
+ */
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (int)n; i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)n; i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+int main() {
+    int n, m; cin >> n >> m;
+    vector<ll> x(n);
+    rep(i, n) cin >> x[i];
+    vector<ll> bonus(n + 1);
+    rep(i, m) {
+        ll c; cin >> c;
+        cin >> bonus[c];
+    }
+    
+    vector dp(n + 1, vector<ll>(n + 1, -1));
+    dp[0][0] = 0;
+    rep2(i, 1, n + 1) {
+        rep(j, n + 1) {
+            if (j == 0) {
+                rep(k, n + 1) chmax(dp[i][j], dp[i - 1][k]);
+            } else {
+                if (dp[i - 1][j - 1] != -1)
+                    dp[i][j] = dp[i - 1][j - 1] + x[i - 1] + bonus[j];
+            }
+        }
+    }
+    cout << *max_element(dp[n].begin(), dp[n].end()) << endl;
+}
