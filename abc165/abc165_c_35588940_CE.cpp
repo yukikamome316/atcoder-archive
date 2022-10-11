540352bf2e78700262560895caa75c28ba3a166f
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2022-10-11 21:06:08 +0900

    [CE] SubmissionID: 35588940 at 2022-10-11 21:06:08 (1665489968)

---
+++ abc165/abc165_c_35588940_CE.cpp
@@ -0,0 +1,43 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/abc165/submissions/35588940
+ * Submitted at: 2022-10-11 21:06:08
+ * Problem URL: https://atcoder.jp/contests/abc165/tasks/abc165_c
+ * Result: CE
+ * Execution Time: None ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+define rep(i,n) for(int i=0;i<n;i++)
+int f(int dep, vector<int> A, int M, vector<vector<int>> data){
+  if(dep==0){
+    int point = 0;
+    rep(i, data.size()){
+      if(A[data[i][1]] - A[data[i][0]]==data[i][2])
+        point += data[i][3];
+    }
+    return point;
+  } 
+
+  int lastA;
+  int point=0;
+  if(A.empty()) lastA=1;
+  else lastA=A.back();
+  for(int i=lastA; i<=M; i++) {
+    A.push_back(i);
+    point += f(dep-1, A, M, data);
+  }
+  return point;
+}
+
+int main(){
+  int N, M, Q; cin >> N  >> M >> Q;
+  vector data(Q, vector<int>(4));
+  rep(i, Q){
+    rep(j, 4){
+      cin >> data[i][j];
+    }
+  }
+  cout << f(N, {}, M, data) << endl;
+}
