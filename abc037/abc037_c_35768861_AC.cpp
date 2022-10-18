0313d60ee71d3d2e337fa5d963c62570381045eb
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2022-10-18 18:34:31 +0900

    [AC] SubmissionID: 35768861 at 2022-10-18 18:34:31 (1666085671)

---
+++ abc037/abc037_c_35768861_AC.cpp
@@ -0,0 +1,31 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/abc037/submissions/35768861
+ * Submitted at: 2022-10-18 18:34:31
+ * Problem URL: https://atcoder.jp/contests/abc037/tasks/abc037_c
+ * Result: AC
+ * Execution Time: 41 ms
+ */
+
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)n; i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    int N, K; cin >> N >> K;
+    vector<ll> a(N); rep(i, N) cin >> a[i];
+
+    vector<ll> s(N + 1);
+    s[0] = 0;
+    rep(i, N) {
+        s[i + 1] = s[i] + a[i];
+    }
+    ll result = 0;
+    rep(i, N - K + 1) {
+        result += s[i + K] - s[i];
+    }
+    cout << result << endl;
+
+}
