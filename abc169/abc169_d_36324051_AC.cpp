35af709428a450e6d759a62251d3af4fb761c9ff
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2022-11-08 19:44:01 +0900

    [AC] SubmissionID: 36324051 at 2022-11-08 19:44:01 (1667904241)

---
+++ abc169/abc169_d_36324051_AC.cpp
@@ -0,0 +1,39 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/abc169/submissions/36324051
+ * Submitted at: 2022-11-08 19:44:01
+ * Problem URL: https://atcoder.jp/contests/abc169/tasks/abc169_d
+ * Result: AC
+ * Execution Time: 16 ms
+ */
+
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)n; i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    ll N; cin >> N;
+
+    vector<pair<ll, ll>> factorize;
+    for (ll i = 2; i * i <= N; i++) {
+        int cnt = 0;
+        while (N % i == 0) {
+            cnt++;
+            N /= i;
+        }
+        if (cnt != 0) factorize.push_back({i, cnt});
+    }
+    if (N != 1) factorize.push_back({N, 1});
+
+    int result = 0;
+    for (auto [p, e] : factorize) {
+        int e_tmp = 1;
+        while (e_tmp * (e_tmp + 1) / 2 <= e) {
+            result++, e_tmp++;
+        }
+    }
+
+    cout << result << endl;
+}
