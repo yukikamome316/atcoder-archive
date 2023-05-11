caa3b11ea44e88ae81bd6c9402bce508961e75fa
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-05-11 18:36:37 +0900

    [AC] SubmissionID: 41304525 at 2023-05-11 18:36:37 (1683797797)

---
+++ abc128/abc128_a_41304525_AC.cpp
@@ -0,0 +1,20 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/abc128/submissions/41304525
+ * Submitted at: 2023-05-11 18:36:37
+ * Problem URL: https://atcoder.jp/contests/abc128/tasks/abc128_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    int A, P; cin >> A >> P;
+
+    cout << (3 * A + P) / 2 << endl;
+}
