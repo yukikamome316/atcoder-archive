83bdb9ab905c3cc7490f2d1c9591bde614538b63
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-05-11 19:13:03 +0900

    [AC] SubmissionID: 41305116 at 2023-05-11 19:13:03 (1683799983)

---
+++ abc198/abc198_a_41305116_AC.cpp
@@ -0,0 +1,21 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/abc198/submissions/41305116
+ * Submitted at: 2023-05-11 19:13:03
+ * Problem URL: https://atcoder.jp/contests/abc198/tasks/abc198_a
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+
+
+int main() {
+    int N; cin >> N;
+    cout << N - 1 << endl;
+}
