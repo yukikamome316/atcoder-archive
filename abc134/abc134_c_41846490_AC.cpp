c6693de4c2aed542d87b1d4291ac57f4a18644d0
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-05-30 14:39:05 +0900

    [AC] SubmissionID: 41846490 at 2023-05-30 14:39:05 (1685425145)

---
+++ abc134/abc134_c_41846490_AC.cpp
@@ -0,0 +1,28 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/abc134/submissions/41846490
+ * Submitted at: 2023-05-30 14:39:05
+ * Problem URL: https://atcoder.jp/contests/abc134/tasks/abc134_c
+ * Result: AC
+ * Execution Time: 326 ms
+ */
+
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    rep(i, N) cin >> A[i];
+
+    vector<int> s(N + 1, -1), e(N + 1, -1);
+    rep(i, N) s[i + 1] = max(s[i], A[i]);
+    rep(i, N) e[i + 1] = max(e[i], A[N - i - 1]);
+
+    rep(i, N)
+        cout << max(s[i], e[N - i - 1]) << endl;
+}
