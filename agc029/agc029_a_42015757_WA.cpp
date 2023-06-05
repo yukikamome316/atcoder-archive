9002db1378d1b15f3b67f644066bb56efe5396ab
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-06-05 16:23:38 +0900

    [WA] SubmissionID: 42015757 at 2023-06-05 16:23:38 (1685949818)

---
+++ agc029/agc029_a_42015757_WA.cpp
@@ -0,0 +1,27 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/agc029/submissions/42015757
+ * Submitted at: 2023-06-05 16:23:38
+ * Problem URL: https://atcoder.jp/contests/agc029/tasks/agc029_a
+ * Result: WA
+ * Execution Time: 12 ms
+ */
+
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  string S;
+  cin >> S;
+
+  int ans = 0, b_cnt = 0;
+  rep(i, S.size()) {
+    if (S[i] == 'W') ans += b_cnt;
+    else if (S[i] == 'B') b_cnt++;
+  }
+
+  cout << ans << endl;
+}
