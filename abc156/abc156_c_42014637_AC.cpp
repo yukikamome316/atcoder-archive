2f33c67bc103fb56b5acd84574a486ec188f7172
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-06-05 15:35:21 +0900

    [AC] SubmissionID: 42014637 at 2023-06-05 15:35:21 (1685946921)

---
+++ abc156/abc156_c_42014637_AC.cpp
@@ -0,0 +1,32 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/abc156/submissions/42014637
+ * Submitted at: 2023-06-05 15:35:21
+ * Problem URL: https://atcoder.jp/contests/abc156/tasks/abc156_c
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+  vector<int> X(N);
+  rep(i, N) cin >> X[i];
+
+  int mean = accumulate(X.begin(), X.end(), 0) / N;
+  int stamina_sum_left = 0, stamina_sum_right = 0;
+  rep(i, N) {
+    int diff_left = X[i] - mean;
+    int diff_right = X[i] - (mean + 1);
+    stamina_sum_left += diff_left*diff_left;
+    stamina_sum_right += diff_right*diff_right;
+  }
+
+  cout << min(stamina_sum_left, stamina_sum_right) << endl;
+}
