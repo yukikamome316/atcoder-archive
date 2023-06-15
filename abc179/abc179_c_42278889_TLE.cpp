cfcb22a2c8f1325310a079dffe446891b4d1d6f3
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-06-15 22:30:08 +0900

    [TLE] SubmissionID: 42278889 at 2023-06-15 22:30:08 (1686835808)

---
+++ abc179/abc179_c_42278889_TLE.cpp
@@ -0,0 +1,30 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/abc179/submissions/42278889
+ * Submitted at: 2023-06-15 22:30:08
+ * Problem URL: https://atcoder.jp/contests/abc179/tasks/abc179_c
+ * Result: TLE
+ * Execution Time: 2205 ms
+ */
+
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+
+  ll cnt = 0;
+  for (int c = 1; c < N; c++) {
+    for (int a = 1; a <= sqrt(N - c); a++) {
+      if ((N - c) % a != 0) continue;
+      if (a * a == N - c) cnt++;
+      else cnt += 2;
+    }
+  }
+
+  cout << cnt << endl;
+}
