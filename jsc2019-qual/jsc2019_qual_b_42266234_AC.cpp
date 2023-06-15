0cf2a6e36580d26589c288a7794d565aa3a887a5
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-06-15 10:51:42 +0900

    [AC] SubmissionID: 42266234 at 2023-06-15 10:51:42 (1686793902)

---
+++ jsc2019-qual/jsc2019_qual_b_42266234_AC.cpp
@@ -0,0 +1,53 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/jsc2019-qual/submissions/42266234
+ * Submitted at: 2023-06-15 10:51:42
+ * Problem URL: https://atcoder.jp/contests/jsc2019-qual/tasks/jsc2019_qual_b
+ * Result: AC
+ * Execution Time: 13 ms
+ */
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)(n); i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main() {
+  int N, K;
+  cin >> N >> K;
+  vector<int> A(N);
+  rep(i, N)
+    cin >> A[i];
+
+  vector<int> B(2 * N);
+  rep(i, 2 * N)
+    B[i] = A[i % N];
+
+  int max_a = *max_element(A.begin(), A.end());
+  fenwick_tree<ll> fw_a(max_a + 1), fw_b(max_a + 1);
+  ll inv_a = 0, inv_b = 0;
+  rep(i, N) {
+    // 重複がある場合、「自分より前にある同じ要素」をカウントしないようにした
+    inv_a += i - fw_a.sum(0, A[i] + 1);
+    fw_a.add(A[i], 1);
+  }
+
+  rep(i, 2 * N) {
+    inv_b += i - fw_b.sum(0, B[i] + 1);
+    fw_b.add(B[i], 1);
+  }
+
+  inv_b -= inv_a * 2;
+
+  modint1000000007 base = inv_a * K;
+  modint1000000007 sub = K - 1;
+  sub *= K;
+  sub *= inv_b;
+  sub /= 2;
+
+  modint1000000007 ans = base + sub;
+  cout << ans.val() << endl;
+}
