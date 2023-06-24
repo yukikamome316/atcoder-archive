593d372a8d7b6988969a746c69f1ad77b0deff09
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-06-24 21:18:07 +0900

    [AC] SubmissionID: 42904622 at 2023-06-24 21:18:07 (1687609087)

---
+++ abc307/abc307_b_42904622_AC.cpp
@@ -0,0 +1,39 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/abc307/submissions/42904622
+ * Submitted at: 2023-06-24 21:18:07
+ * Problem URL: https://atcoder.jp/contests/abc307/tasks/abc307_b
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+  vector<string> S(N);
+  rep(i, N) cin >> S[i];
+
+  rep(i, N) {
+    rep(j, N) {
+      if (i == j) continue;
+
+      string word = S[i] + S[j];
+      bool ok = true;
+      rep(k, word.size()) {
+        if (word[k] != word[word.size() - k - 1])
+          ok = false;
+      }
+      if (ok) {
+        cout << "Yes" << endl;
+        return 0;
+      }
+    }
+  }
+  cout << "No" << endl;
+}
