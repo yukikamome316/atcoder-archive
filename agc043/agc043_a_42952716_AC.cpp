0992f028fdbca7698ce806550aa5d83ec97fc346
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-06-25 17:23:24 +0900

    [AC] SubmissionID: 42952716 at 2023-06-25 17:23:24 (1687681404)

---
+++ agc043/agc043_a_42952716_AC.cpp
@@ -0,0 +1,43 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/agc043/submissions/42952716
+ * Submitted at: 2023-06-25 17:23:24
+ * Problem URL: https://atcoder.jp/contests/agc043/tasks/agc043_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+int main() {
+  int H, W;
+  cin >> H >> W;
+  vector<string> fi(H);
+  rep(i, H)
+    cin >> fi[i];
+
+  vector dp(H, vector<int>(W, 2e9));
+  dp[0][0] = (fi[0][0] == '#');
+
+  vector<int> dx = {0, 1};
+  vector<int> dy = {1, 0};
+
+  rep(i, H) {
+    rep(j, W) {
+      rep(dir, 2) {
+        int nx = i + dx[dir], ny = j + dy[dir];
+        if (nx >= H || ny >= W) continue;
+
+        chmin(dp[nx][ny], dp[i][j] + (fi[i][j] == '.' && fi[nx][ny] == '#'));
+      }
+    }
+  }
+
+  cout << dp[H - 1][W - 1] << endl;
+}
