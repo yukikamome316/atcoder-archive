e1bb5d992a10c7c1d012b8de0dc7dc963d1f9539
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-07-03 00:22:05 +0900

    [AC] SubmissionID: 43204359 at 2023-07-03 00:22:05 (1688311325)

---
+++ abc157/abc157_c_43204359_AC.cpp
@@ -0,0 +1,49 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/abc157/submissions/43204359
+ * Submitted at: 2023-07-03 00:22:05
+ * Problem URL: https://atcoder.jp/contests/abc157/tasks/abc157_c
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N, M;
+  cin >> N >> M;
+  vector<char> s(N, '.');
+  rep(i, M) {
+    int x, y;
+    cin >> x >> y;
+    if (s[x - 1] != '.' && s[x - 1] != y + '0') {
+      cout << -1 << endl;
+      return 0;
+    }
+    s[x - 1] = y + '0';
+  }
+
+  if (s[0] == '0' && N >= 2) {
+    cout << -1 << endl;
+    return 0;
+  }
+
+  if (s[0] == '.' && N >= 2) {
+    s[0] = '1';
+  }
+
+  rep(i, N) {
+    if (s[i] == '.') {
+      s[i] = '0';
+    }
+  }
+
+  rep(i, N) {
+    cout << s[i];
+  }
+  cout << endl;
+}
