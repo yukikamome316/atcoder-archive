799ff6edb6d74ad3573bf14d6d0ab5576b3e357c
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-07-08 21:06:16 +0900

    [AC] SubmissionID: 43344242 at 2023-07-08 21:06:16 (1688817976)

---
+++ abc309/abc309_a_43344242_AC.cpp
@@ -0,0 +1,22 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/abc309/submissions/43344242
+ * Submitted at: 2023-07-08 21:06:16
+ * Problem URL: https://atcoder.jp/contests/abc309/tasks/abc309_a
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+  cout << (B - 1 == A && B != 1 && B != 4 && B != 7 ? "Yes" : "No") << endl;
+}
