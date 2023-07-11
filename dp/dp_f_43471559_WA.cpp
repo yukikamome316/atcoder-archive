1073c9270867a99ea06eba9297d8a658ff34ae00
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-07-11 10:17:31 +0900

    [WA] SubmissionID: 43471559 at 2023-07-11 10:17:31 (1689038251)

---
+++ dp/dp_f_43471559_WA.cpp
@@ -0,0 +1,58 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/dp/submissions/43471559
+ * Submitted at: 2023-07-11 10:17:31
+ * Problem URL: https://atcoder.jp/contests/dp/tasks/dp_f
+ * Result: WA
+ * Execution Time: 137 ms
+ */
+
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+int main() {
+  string s, t;
+  cin >> s >> t;
+
+  vector dp(s.size() + 1, vector<ll>(t.size() + 1));
+  vector dp_prev(s.size() + 1, vector<pair<int, int>>(t.size() + 1, {-1, -1}));
+
+  for (int i = 1; i < s.size() + 1; i++) {
+    for (int j = 1; j < t.size() + 1; j++) {
+      dp[i][j] = dp[i - 1][j];
+      dp_prev[i][j] = {i - 1, j};
+
+      if (dp[i][j] < dp[i][j - 1]) {
+        dp[i][j] = dp[i][j - 1];
+        dp_prev[i][j] = {i, j - 1};
+      }
+
+      if (s[i - 1] == t[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+        dp_prev[i][j] = {i - 1, j - 1};
+      }
+    }
+  }
+
+  if (dp[s.size()][t.size()] == 0) {
+    cout << endl;
+    return 0;
+  }
+
+  pair<int, int> current = {s.size(), t.size()};
+  string result = "";
+
+  while (!(current.first == -1 && current.second == -1)) {
+    if (s[current.first] == t[current.second]) {
+      result.insert(result.begin(), s[current.first]);
+    }
+    current = dp_prev[current.first][current.second];
+  }
+
+  cout << result << endl;
+}
