048d6b1c545d3fc94b670e16656528c07d87c73a
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-07-20 19:21:57 +0900

    [AC] SubmissionID: 43776566 at 2023-07-20 19:21:57 (1689848517)

---
+++ jsc2021/jsc2021_d_43776566_AC.cpp
@@ -0,0 +1,29 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/jsc2021/submissions/43776566
+ * Submitted at: 2023-07-20 19:21:57
+ * Problem URL: https://atcoder.jp/contests/jsc2021/tasks/jsc2021_d
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)(n); i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+int main() {
+  int N, P;
+  cin >> N >> P;
+  modint1000000007 ans = (P-1);
+  vector<modint1000000007> r(70,P-2);
+  rep(i,69) r[i+1] = r[i] * r[i];
+  N--;
+  rep(i,40) if( (N & (1LL << i)) ) ans *= r[i];
+  cout << ans.val() << endl;
+}
