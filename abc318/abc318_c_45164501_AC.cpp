0d964c53eb6ee068d3298d8dadd5b9cbbf7db193
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-09-02 21:45:33 +0900

    [AC] SubmissionID: 45164501 at 2023-09-02 21:45:33 (1693658733)

---
+++ abc318/abc318_c_45164501_AC.cpp
@@ -0,0 +1,49 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/abc318/submissions/45164501
+ * Submitted at: 2023-09-02 21:45:33
+ * Problem URL: https://atcoder.jp/contests/abc318/tasks/abc318_c
+ * Result: AC
+ * Execution Time: 69 ms
+ */
+
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+int main() {
+  int N, D, P;
+  cin >> N >> D >> P;
+  // vector<int> F(N);
+  // rep(i, N) cin >> F[i];
+
+  priority_queue<int> pq;
+  rep(i, N) {
+    int tmp;
+    cin >> tmp;
+    pq.push(tmp);
+  }
+
+  ll ans = 0;
+
+  while (!pq.empty()) {
+    ll sum = 0;
+
+    int cnt = 0;
+    while (cnt < D && !pq.empty()) {
+      int x = pq.top();
+      pq.pop();
+      sum += x;
+      cnt++;
+    }
+
+    if (sum < P) ans += sum;
+    else ans += P;
+  }
+
+  cout << ans << endl;
+}
