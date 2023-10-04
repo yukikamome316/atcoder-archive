43219f7d90a9a5b46496fcc41441d0051630dd54
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-10-04 18:45:27 +0900

    [AC] SubmissionID: 46210426 at 2023-10-04 18:45:27 (1696412727)

---
+++ abc063/arc075_a_46210426_AC.cpp
@@ -0,0 +1,44 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/abc063/submissions/46210426
+ * Submitted at: 2023-10-04 18:45:27
+ * Problem URL: https://atcoder.jp/contests/abc063/tasks/arc075_a
+ * Result: AC
+ * Execution Time: 2 ms
+ */
+
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+int main() {
+  int N;
+  cin >> N;
+  vector<int> s(N);
+  rep(i, N) cin >> s[i];
+
+  vector<vector<bool>> dp(N + 1, vector<bool>(100 * 100 + 1));
+  // dp[i][j]: i個目までを使ってj点を作れるか
+  
+  dp[0][0] = true;
+
+  rep(i, N) {
+    rep(j, 100 * 100 + 1) {
+      if (dp[i][j]) {
+        dp[i + 1][j] = true;      
+        dp[i + 1][j + s[i]] = true;
+      }
+    }
+  }
+
+  int ans = 0;
+  rep(i, 100 * 100 + 1) {
+    if (i % 10 != 0 && dp[N][i]) chmax(ans, i);
+  }
+  
+  cout << ans << endl;
+}
