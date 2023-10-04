e5f65cddbe57571832935ca671b7a665b4271451
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-10-04 18:30:14 +0900

    [WA] SubmissionID: 46210135 at 2023-10-04 18:30:14 (1696411814)

---
+++ abc063/arc075_a_46210135_WA.cpp
@@ -0,0 +1,33 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/abc063/submissions/46210135
+ * Submitted at: 2023-10-04 18:30:14
+ * Problem URL: https://atcoder.jp/contests/abc063/tasks/arc075_a
+ * Result: WA
+ * Execution Time: 1 ms
+ */
+
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+int main() {
+  int N;
+  cin >> N;
+  vector<int> s(N);
+  rep(i, N) cin >> s[i];
+
+  sort(s.rbegin(), s.rend());
+
+  int ans = 0;
+  rep(i, N) {
+    int tmp = ans + s[i];
+    if (tmp % 10 != 0) ans = tmp;
+  }
+
+  cout << ans << endl;
+}
