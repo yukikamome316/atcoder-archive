420fc342495dc73934f48281043b50b219642f2f
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-10-12 18:22:37 +0900

    [WA] SubmissionID: 46473375 at 2023-10-12 18:22:37 (1697102557)

---
+++ aising2020/aising2020_b_46473375_WA.cpp
@@ -0,0 +1,28 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/aising2020/submissions/46473375
+ * Submitted at: 2023-10-12 18:22:37
+ * Problem URL: https://atcoder.jp/contests/aising2020/tasks/aising2020_b
+ * Result: WA
+ * Execution Time: 1 ms
+ */
+
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+int main() {
+  int N;
+  cin >> N;
+  int cnt = 0;
+  for (int i = 0; i < N; i++) {
+    int x;
+    cin >> x;
+    if (x % 2 && i % 2) cnt++;
+  }
+  cout << cnt << endl;
+}
