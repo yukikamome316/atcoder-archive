2c04708256ec4ff98aca905a4f0aadc59f145f4d
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-10-27 10:08:06 +0900

    [AC] SubmissionID: 46957042 at 2023-10-27 10:08:06 (1698368886)

---
+++ abc323/abc323_d_46957042_AC.cpp
@@ -0,0 +1,41 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/abc323/submissions/46957042
+ * Submitted at: 2023-10-27 10:08:06
+ * Problem URL: https://atcoder.jp/contests/abc323/tasks/abc323_d
+ * Result: AC
+ * Execution Time: 1032 ms
+ */
+
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+int main() {
+  int N;
+  cin >> N;
+  map<ll, ll> cnt;
+  rep(i, N) {
+    ll si;
+    ll ci;
+    cin >> si >> ci;
+    cnt[si] += ci;
+  }
+
+  ll ans = 0;
+  auto itr = cnt.begin();
+  while (itr != cnt.end()) {
+    ll s = itr->first;
+    ll c = itr->second;
+
+    if (c > 1) cnt[2 * s] += c / 2;
+    if (c & 1) ans++;
+    itr++;
+  }
+
+  cout << ans << endl;
+}
