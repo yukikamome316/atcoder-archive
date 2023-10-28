08843b0f1326050a5be0c7152020aea6559db698
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-10-28 21:53:39 +0900

    [AC] SubmissionID: 47030455 at 2023-10-28 21:53:39 (1698497619)

---
+++ abc326/abc326_c_47030455_AC.cpp
@@ -0,0 +1,57 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/abc326/submissions/47030455
+ * Submitted at: 2023-10-28 21:53:39
+ * Problem URL: https://atcoder.jp/contests/abc326/tasks/abc326_c
+ * Result: AC
+ * Execution Time: 436 ms
+ */
+
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+int main() {
+  int N, M;
+  cin >> N >> M;
+  map<int, int> mp;
+  set<int> st;
+
+  rep(i, N) {
+    int A;
+    cin >> A;
+    mp[A]++;
+    st.insert(A);
+  }
+
+  vector<int> vec(st.begin(), st.end());
+  vector<ll> s(N + 1);
+
+  auto itr = mp.begin();
+  rep(i, N) {
+    s[i + 1] = s[i] + itr->second;
+    itr++;
+  }
+
+  ll ans = 0;
+  int current = 1;
+  for (auto [a, cnt] : mp) {
+    int val = a + M;
+    auto itr = lower_bound(vec.begin(), vec.end(), val);
+    if (itr != vec.begin()) {
+      itr--;
+      chmax(ans, s[itr - vec.begin() + 1] - s[current - 1]);
+    } else {
+      chmax(ans, s[current]);
+    }
+    
+    current++;
+  }
+  
+  cout << ans << endl;
+  
+}
