f6a9e6d96325f713ed253c743c296a7729be5463
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-11-02 14:50:04 +0900

    [WA] SubmissionID: 47149436 at 2023-11-02 14:50:04 (1698904204)

---
+++ abc267/abc267_d_47149436_WA.cpp
@@ -0,0 +1,37 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/abc267/submissions/47149436
+ * Submitted at: 2023-11-02 14:50:04
+ * Problem URL: https://atcoder.jp/contests/abc267/tasks/abc267_d
+ * Result: WA
+ * Execution Time: 18 ms
+ */
+
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+int main() {
+  int N, M;
+  cin >> N >> M;
+  vector<int> A(N);
+  rep(i, N) cin >> A[i];
+  
+  vector<vector<ll>> dp(N + 1, vector<ll>(N + 1));
+  dp[0][0] = 0;
+  dp[0][1] = INT_MIN;
+
+  rep2(i, 1, N + 1) {
+    rep(j, N + 1) {
+      if (j == 0) dp[i][j] = 0;
+      else if (j > i) dp[i][j] = INT_MIN;
+      else dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - 1] + j * A[i - 1]);
+    }
+  }
+
+  cout << dp[N][M] << endl;
+}
