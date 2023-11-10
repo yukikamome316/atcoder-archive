a9a453753a445e12d029b41ddce05093978bcaad
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-11-10 22:05:34 +0900

    [WA] SubmissionID: 47428052 at 2023-11-10 22:05:34 (1699621534)

---
+++ abc130/abc130_d_47428052_WA.cpp
@@ -0,0 +1,41 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/abc130/submissions/47428052
+ * Submitted at: 2023-11-10 22:05:34
+ * Problem URL: https://atcoder.jp/contests/abc130/tasks/abc130_d
+ * Result: WA
+ * Execution Time: 18 ms
+ */
+
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+int main() {
+  int N, K;
+  cin >> N >> K;
+  vector<int> a(N);
+  rep(i, N) cin >> a[i];
+  
+  vector<ll> s(N + 1);
+  rep(i, N) s[i + 1] = s[i] + a[i];
+
+  int right = 0;
+  ll ans = 0;
+
+  rep(left, N + 1) {
+    while (right < N && s[right] - s[left] < K) {
+      right++;
+    }
+
+    if (s[right] - s[left] >= K) ans += N - (right - 1);
+    
+    if (left == right) right++;
+  }
+
+  cout << ans << endl;
+}
