cfc43f532e7bfe19ed6f0de8859e95ec0b532d30
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-11-29 20:05:10 +0900

    [WA] SubmissionID: 48031568 at 2023-11-29 20:05:10 (1701255910)

---
+++ arc142/arc142_a_48031568_WA.cpp
@@ -0,0 +1,49 @@
+/*
+ * Author: yukikamome316
+ * Submission URL: https://atcoder.jp/contests/arc142/submissions/48031568
+ * Submitted at: 2023-11-29 20:05:10
+ * Problem URL: https://atcoder.jp/contests/arc142/tasks/arc142_a
+ * Result: WA
+ * Execution Time: 1 ms
+ */
+
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (int)s; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+int main() {
+  ll N, K;
+  cin >> N >> K;
+
+  set<ll> seen;
+  queue<ll> que;
+  que.push(K);
+
+  ll ans = 0;
+  while (!que.empty()) {
+    ll x = que.front();
+    que.pop();
+
+    if (x < K) break;
+    if (seen.contains(x)) continue;
+    seen.insert(x);
+
+    que.push(x * 10);
+
+    if (x < 1 || x > N) continue;
+    ll rev_val = 0;
+    while (x > 0) {
+      rev_val = rev_val * 10 + x % 10;
+      x /= 10;
+    }
+    que.push(rev_val);
+
+    ans++;
+  }
+
+  cout << ans << endl;
+}
